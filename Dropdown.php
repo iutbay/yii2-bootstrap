4a6bc865fba289a54147f457daf28dfed742e3e3
Kevin LEVRON <kevin.levron@univ-pau.fr>
2015-10-31 14:49:23 +0100

    Create Dropdown.php

---
+++ Dropdown.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace iutbay\yii2bootstrap;
+
+use yii\helpers\ArrayHelper;
+
+use iutbay\yii2fontawesome\FontAwesome as FA;
+
+class Dropdown extends \yii\bootstrap\Dropdown
+{
+
+    /**
+     * Renders the widget.
+     */
+    public function run()
+    {
+        // icon support
+        foreach ($this->items as &$item) {
+            if (isset($item['icon'])) {
+                $icon = FA::icon($item['icon'] . ' fw');
+                $item['label'] = trim($icon . ' ' . ArrayHelper::getValue($item, 'label', ''));
+                $item['encode'] = false;
+                unset($item['icon']);
+            }
+        }
+
+        return parent::run();
+    }
+
+}
