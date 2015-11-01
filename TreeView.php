c5738c1a9fd5a5ad7d055d540afca478fcbe492d
Kevin LEVRON <kevin.levron@univ-pau.fr>
2015-11-01 23:40:37 +0100

    Create TreeView.php

---
+++ TreeView.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace iutbay\yii2bootstrap;
+
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+use iutbay\yii2fontawesome\FontAwesome as FA;
+
+/**
+ * TreeView widget
+ * @author Kevin LEVRON <kevin.levron@gmail.com>
+ */
+class TreeView extends \yii\bootstrap\Widget
+{
+
+}
