1cedf9a8e2b6ee5f39d52e571bc07a67092e2dcf
Kevin LEVRON <kevin.levron@univ-pau.fr>
2015-11-01 23:45:15 +0100

    Create TreeViewAsset.php

---
+++ TreeViewAsset.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace iutbay\yii2bootstrap;
+
+/**
+ * TreeView asset bundle
+ * @author Kevin LEVRON <kevin.levron@gmail.com>
+ */
+class TreeViewAsset extends \yii\web\AssetBundle
+{
+    public $sourcePath = '@vendor/iutbay/yii2-bootstrap/assets';
+    public $js = [
+        'treeview.js',
+    ];
+    public $depends = [
+        'yii\bootstrap\BootstrapPluginAsset',
+        'iutbay\yii2fontawesome\FontAwesomeAsset',
+    ];
+    public $publishOptions = [
+        'forceCopy' => YII_DEBUG,
+    ];
+}
