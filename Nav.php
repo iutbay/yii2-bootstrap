0b981f4522a8a72bd0e6fa07fa8aeadc356d37ba
Kevin LEVRON <kevin.levron@univ-pau.fr>
2015-11-02 22:12:51 +0100

    Update Nav.php

--- Nav.php
+++ Nav.php
@@ -4,12 +4,32 @@ namespace iutbay\yii2bootstrap;
 
 use yii\helpers\ArrayHelper;
 
+use iutbay\yii2fontawesome\FontAwesome as FA;
+
 /**
  * Nav widget
  * @author Kevin LEVRON <kevin.levron@gmail.com>
  */
 class Nav extends \yii\bootstrap\Nav
 {
+
+    /**
+     * Renders the widget.
+     */
+    public function run()
+    {
+        // icon support
+        foreach ($this->items as &$item) {
+            if (isset($item['icon'])) {
+                $icon = FA::icon($item['icon']);
+                $item['label'] = trim($icon . ' ' . ArrayHelper::getValue($item, 'label', ''));
+                $item['encode'] = false;
+                unset($item['icon']);
+            }
+        }
+
+        return parent::run();
+    }
 
     /**
      * @inheritdoc
