2fada7cb781d4ccc711b0f3854fd0ea82f7cb2f0
glop <glop@baloo>
2015-11-03 23:09:26 +0100

    wip

--- TreeView.php
+++ TreeView.php
@@ -6,7 +6,6 @@ use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\helpers\Json;
 use yii\helpers\Url;
-
 use iutbay\yii2fontawesome\FontAwesome as FA;
 
 /**
@@ -17,10 +16,20 @@ class TreeView extends \yii\base\Widget
 {
 
     /**
-     * Items (TODO)
+     * Items
      * @var array
      */
     public $items;
+
+    /**
+     * @var string
+     */
+    public $itemLabelAttribute = 'label';
+
+    /**
+     * @var string
+     */
+    public $itemChildrenAttribute = 'items';
 
     /**
      * List selector
@@ -39,10 +48,91 @@ class TreeView extends \yii\base\Widget
      */
     public function run()
     {
-        $options = Json::encode($this->clientOptions);
+        echo $this->renderItems();
+        $this->registerClientScript();
+    }
+
+    /**
+     * Render items
+     * @return type
+     */
+    public function renderItems()
+    {
+        return $this->renderUL($this->items, ['id' => $this->getId()]);
+    }
+
+    /**
+     * Register js
+     */
+    public function registerClientScript()
+    {
         $view = $this->getView();
         TreeViewAsset::register($view);
-        $view->registerJs("jQuery('{$this->listSelector}').treeview($options);");
+        $id = $this->listSelector === null ? '#' . $this->getId() : $this->listSelector;
+        $options = Json::encode($this->clientOptions);
+        $view->registerJs("jQuery('{$id}').treeview($options);");
+    }
+
+    /**
+     * Render ul
+     * @param array $items
+     * @param array $options
+     * @return string
+     */
+    public function renderUL($items, $options = [])
+    {
+        if (!is_array($items) || count($items) === 0)
+            return;
+
+        $lis = '';
+        foreach ($items as $item) {
+            $lis.= $this->renderLI($item);
+        }
+        return Html::tag('ul', $lis, $options);
+    }
+
+    /**
+     * Render li
+     * @param mixed $item
+     * @param array $options
+     * @return string
+     */
+    public function renderLI($item, $options = [])
+    {
+        $li = $this->getItemLabel($item);
+        $li.= $this->renderUL($this->getItemChildren($item));
+        return Html::tag('li', $li, $options);
+    }
+
+    /**
+     * @param mixed $item
+     * @return string
+     */
+    public function getItemLabel($item)
+    {
+        return $this->getItemAttribute($item, $this->itemLabelAttribute);
+    }
+
+    /**
+     * @param mixed $item
+     * @return array
+     */
+    public function getItemChildren($item)
+    {
+        return $this->getItemAttribute($item, $this->itemChildrenAttribute);
+    }
+
+    /**
+     * @param mixed $item
+     * @return array
+     */
+    public function getItemAttribute($item, $attribute)
+    {
+        if (is_array($item))
+            return $item[$attribute];
+
+        if (is_object($item))
+            return $item->{$attribute};
     }
 
 }
--- assets/treeview.js
+++ assets/treeview.js
@@ -12,26 +12,32 @@
 		this.options = options;
 		this.openIconClass = 'fa-' + this.options.openIcon;
 		this.closeIconClass = 'fa-' + this.options.closeIcon;
-		this.openIconHtml = '<i class="treeview-icon fa fa-fw ' + this.openIconClass + '"></i>';
-		this.closeIconHtml = '<i class="treeview-icon fa fa-fw ' + this.closeIconClass + '"></i>';
+		this.normalIconClass = 'fa-' + this.options.normalIcon;
+		this.openIconHtml = '<i class="treeview-icon open-icon fa fa-fw ' + this.openIconClass + '"></i>';
+		this.closeIconHtml = '<i class="treeview-icon close-icon fa fa-fw ' + this.closeIconClass + '"></i>';
+		this.normalIconHtml = '<i class="treeview-icon fa fa-fw ' + this.normalIconClass + '"></i>';
 
 		var treeview = this,
 			jtree = this.jtree;
 
         // init treeview
 		jtree.addClass('treeview');
-		jtree.find('li').has('ul').each(function () {
-			var jbranch = $(this);
-            jbranch.addClass('treeview-branch');
-            if (treeview.init === 'collapsed') {
-                jbranch.prepend(treeview.openIconHtml).find('> ul').slideToggle('fast');
-            } else {
-                jbranch.prepend(treeview.closeIconHtml);
-            }
+		jtree.find('li').each(function () {
+			var jthis = $(this);
+			if (jthis.find('> ul').length) {
+				jthis.addClass('treeview-branch');
+				if (treeview.init === 'collapsed') {
+					jthis.prepend(treeview.openIconHtml).find('> ul').slideToggle('fast');
+				} else {
+					jthis.prepend(treeview.closeIconHtml);
+				}
+			} else {
+				jthis.prepend(treeview.normalIconHtml);
+			}
 		});
 
         // click handler
-		jtree.on('click', '.treeview-icon', function (e) {
+		jtree.on('click', '.open-icon, .close-icon', function (e) {
 			var jicon = $(this), jli = jicon.parent();
 			jicon.toggleClass(treeview.openIconClass + " " + treeview.closeIconClass);
 			jli.find('> ul').slideToggle('fast');
@@ -43,10 +49,9 @@
 	 */
 	Treeview.DEFAULTS = {
         init: 'collapsed',
-//		openIcon: 'plus-square-o',
-//		closeIcon: 'minus-square-o'
 		openIcon: 'folder-o',
-		closeIcon: 'folder-open-o'
+		closeIcon: 'folder-open-o',
+		normalIcon: 'file-o'
 	};
 
 	/**
