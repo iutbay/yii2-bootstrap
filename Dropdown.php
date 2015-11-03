c7e17e0dc79e6128d00136c438184777428c9efd
Kevin LEVRON <kevin.levron@univ-pau.fr>
2015-11-03 14:40:21 +0100

    wip

--- Dropdown.php
+++ Dropdown.php
@@ -3,6 +3,7 @@
 namespace iutbay\yii2bootstrap;
 
 use yii\helpers\ArrayHelper;
+use yii\bootstrap\Html;
 
 use iutbay\yii2fontawesome\FontAwesome as FA;
 
@@ -14,17 +15,29 @@ class Dropdown extends \yii\bootstrap\Dropdown
 {
 
     /**
-     * Renders the widget.
+     * @inheritdoc
      */
     public function run()
     {
-        // icon support
         foreach ($this->items as &$item) {
+            // icon ?
             if (isset($item['icon'])) {
                 $icon = FA::icon($item['icon'] . ' fw');
                 $item['label'] = trim($icon . ' ' . ArrayHelper::getValue($item, 'label', ''));
                 $item['encode'] = false;
                 unset($item['icon']);
+            }
+
+            // active ?
+            if (isset($item['active'])) {
+                $active = $item['active'];
+                if ($active instanceof \Closure) {
+                    $active = call_user_func($active, $this->getView()->getContext());
+                }
+                if ($active) {
+                    Html::addCssClass($item['options'], 'active');
+                }
+                unset($item['active']);
             }
         }
 
